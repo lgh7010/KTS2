d7a6e50f8bd0e3627c431890d20e658c95e05580
lgh7010 <lgh7010@krafton.com>
2020-09-23 16:18:14 +0900

    프로젝트 초기화

---
+++ src/test/java/com/krafton/kts/KtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krafton.kts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

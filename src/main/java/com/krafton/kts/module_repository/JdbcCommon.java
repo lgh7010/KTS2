4bbe272ce085a09f62059662c4b6944cd881fa50
lgh7010 <lgh7010@krafton.com>
2020-09-23 17:31:19 +0900

    JdbcCommon에 누락된 abstract 추가

--- src/main/java/com/krafton/kts/module_repository/JdbcCommon.java
+++ src/main/java/com/krafton/kts/module_repository/JdbcCommon.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class JdbcCommon {
+public abstract class JdbcCommon {
 
     private final DataSource dataSource;
 

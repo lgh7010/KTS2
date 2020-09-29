b6a0b9c7692e0b8c22ca32b9fef9f47d40eeaa41
lgh7010 <lgh7010@krafton.com>
2020-09-29 15:05:22 +0900

    단순히 webpack을 적용하는 문제가 아니었다. 프론트앤드의 기존 내 개발 패러다임 자체를 바꿔야 한다. html파일을 작성하는것이 아니며, main.js같은 메인 자바스크립트가 있고, 여기서 모듈들을 이용해 html을 만들어나간다.

--- frontend/src/main/resources/static/js/testList.js
+++ frontend/src/main/resources/static/js/testList.js
@@ -47,6 +47,32 @@ function closeEditLayer(){
 
 
 
+var testcaseListVue = null
+function setTestcaseListVue(testRelTestcaseList, testcaseDic){
+    $("#testRelTestcaseList").show();
+    var list = [];
+    for(var i = 0; i < testRelTestcaseList.length; i++){
+        list.push({
+            RELATION_SEQ: testRelTestcaseList[i].relation_SEQ,
+            TEST_SEQ : testRelTestcaseList[i].test_SEQ,
+            LIST_INDEX : testRelTestcaseList[i].list_INDEX,
+            TESTCASE_SEQ : testRelTestcaseList[i].testcase_SEQ,
+            NAME : testcaseDic[testRelTestcaseList[i].testcase_SEQ].name,
+            DESCRIPTION : testcaseDic[testRelTestcaseList[i].testcase_SEQ].description
+        })
+    }
+
+    if(testcaseListVue == null){
+        testcaseListVue = new Vue({
+            el : "#testRelTestcaseList",
+            data : {
+                testRelTestcaseList_with_name_and_desc : list
+            }
+        })
+    } else {
+        testcaseListVue.testRelTestcaseList_with_name_and_desc = list;
+    }
+}
 
 
 

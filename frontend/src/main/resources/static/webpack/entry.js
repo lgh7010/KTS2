3a38ea816c4207e333c75d6277a759f74f178142
lgh7010 <lgh7010@krafton.com>
2020-10-06 17:14:17 +0900

    필요한것만 남김

--- frontend/src/main/resources/static/webpack/entry.js
+++ frontend/src/main/resources/static/webpack/entry.js
@@ -5,8 +5,6 @@ import App from './App.vue'
 new Vue({
     el: '#app',
     render: h => h(App),
-    components: { App },
-    template: '<App/>',
     created: function(){
         console.log("vue is created");
         this.drawTestList()
--- frontend/src/main/resources/static/webpack/webpack.config.js
+++ frontend/src/main/resources/static/webpack/webpack.config.js
@@ -1,5 +1,4 @@
-const path = require('path')
-const {VueLoaderPlugin} = require('vue-loader')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
     mode: 'development',

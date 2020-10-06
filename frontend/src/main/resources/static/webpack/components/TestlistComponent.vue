157594e7d37521c57fda8c851cbc99c8ff93fb98
lgh7010 <lgh7010@krafton.com>
2020-10-06 18:39:24 +0900

    vue에서 뿌려주는게 DB값이 아니라 임의 데이터던 문제 수정

--- frontend/src/main/resources/static/webpack/components/TestlistComponent.vue
+++ frontend/src/main/resources/static/webpack/components/TestlistComponent.vue
@@ -4,7 +4,7 @@
 
     </thead>
     <tbody id="testList">
-      <tr v-for="test in testList">
+      <tr v-for="test in this.testList">
         <td>{{test.test_SEQ}}</td>
         <td>{{test.name}}</td>
         <td>{{test.description}}</td>
@@ -16,17 +16,21 @@
 
 <script>
 import axios from 'axios'
-import Vue from 'vue'
 
 export default {
   name: 'testlistComponent',
+  data: function(){
+    return {
+      testList: []
+    }
+  },
   created: function(){
 
   },
   mounted: function(){
     axios.get('/testListOnly').then(testList => {
       console.log(testList.data)
-      Vue.prototype.testList = testList.data
+      this.testList = testList.data
     }).catch(error => {
       console.log(error)
     })
--- frontend/src/main/resources/static/webpack/entry.js
+++ frontend/src/main/resources/static/webpack/entry.js
@@ -6,24 +6,6 @@ import testlistComponent from "./components/TestlistComponent.vue"
 
 Vue.component('testlistComponent', testlistComponent)
 
-Vue.prototype.testList = [
-    {
-        'test_SEQ': 1,
-        'name': 'test1',
-        'description': 'test1desc'
-    },
-    {
-        'test_SEQ': 2,
-        'name': 'test2',
-        'description': 'test2desc'
-    },
-    {
-        'test_SEQ': 3,
-        'name': 'test3',
-        'description': 'test3desc'
-    }
-]
-
 new Vue({
     el: '#app',
     render: h => h(App),

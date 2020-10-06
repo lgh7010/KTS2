9f83d4461ef87294f537432a622687a8930015bf
lgh7010 <lgh7010@krafton.com>
2020-10-06 18:56:05 +0900

    웹펙으로 만든 vue에 버튼 구현

--- frontend/src/main/resources/static/webpack/components/TestlistComponent.vue
+++ frontend/src/main/resources/static/webpack/components/TestlistComponent.vue
@@ -1,13 +1,22 @@
 <template>
   <table>
     <thead>
-
+      <tr>
+        <th>순서</th>
+        <th>테스트케이스 이름</th>
+        <th>설명</th>
+        <th>상호작용</th>
+      </tr>
     </thead>
     <tbody id="testList">
       <tr v-for="test in this.testList">
         <td>{{test.test_SEQ}}</td>
         <td>{{test.name}}</td>
         <td>{{test.description}}</td>
+        <td>
+          <button v-on:click="onClickEdit(test.test_SEQ)">편집</button>
+          <button v-on:click="onClickRemove(test.test_SEQ)">삭제</button>
+        </td>
       </tr>
 
     </tbody>
@@ -24,16 +33,20 @@ export default {
       testList: []
     }
   },
-  created: function(){
-
-  },
   mounted: function(){
     axios.get('/testListOnly').then(testList => {
-      console.log(testList.data)
       this.testList = testList.data
     }).catch(error => {
       console.log(error)
     })
+  },
+  methods: {
+    onClickEdit: function(TEST_SEQ){
+      console.log(TEST_SEQ);
+    },
+    onClickRemove: function(TEST_SEQ){
+      console.log(TEST_SEQ);
+    }
   }
 }
 </script>

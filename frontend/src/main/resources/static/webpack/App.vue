8b0819f1e065f9905b2230814a40c5bb37230b53
lgh7010 <lgh7010@krafton.com>
2020-10-07 16:00:11 +0900

    사이드바, 헤더 컴포넌트 추가. 라우터를 추가하기 위한 준비 완료

--- frontend/src/main/resources/static/webpack/App.vue
+++ frontend/src/main/resources/static/webpack/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app2">
+    <header-component/>
+    <sidebar-component/>
+
     <test-list-component/>
     <test-edit-component/>
   </div>
@@ -8,10 +11,14 @@
 <script>
 import testListComponent from './components/TestListComponent.vue'
 import testEditComponent from './components/TestEditComponent.vue'
+import SidebarComponent from "./components/SidebarComponent.vue"
+import HeaderComponent from "./components/HeaderComponent.vue"
 
 export default {
   name: 'App',
   components: {
+    HeaderComponent,
+    SidebarComponent,
     testListComponent,
     testEditComponent
   }
---
+++ frontend/src/main/resources/static/webpack/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    이건 헤더
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent"
+}
+</script>
+
+<style scoped>
+
+</style>

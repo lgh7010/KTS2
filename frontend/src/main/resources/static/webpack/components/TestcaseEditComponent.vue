5f1ba5f30a06c97d8f95d6c8bbdb081d6576060b
lgh7010 <lgh7010@krafton.com>
2020-10-08 16:19:00 +0900

    최초 드로잉 성공

--- frontend/src/main/resources/static/webpack/components/TestcaseEditComponent.vue
+++ frontend/src/main/resources/static/webpack/components/TestcaseEditComponent.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
     <div>
-      <table>
-        <thead>
-        <tr>
-          <th>액션ID</th>
-          <th>설명</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="action in this.actionDic">
-          <td>{{action.action_ID}}</td>
-          <td>{{action.description}}</td>
-        </tr>
-        </tbody>
-      </table>
+      <svg ref="box" width="500" height="500">
+        <rect width="100%" height="100%" fill="white" stroke="black" stroke-width="5"></rect>
+        <rect width="100" height="100" class="square" fill="red" :x="square.x" :y="square.y" @mousedown="drag" @mouseup="drop"></rect>
+      </svg>
     </div>
+
+    <table>
+      <thead>
+      <tr>
+        <th>액션ID</th>
+        <th>설명</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="action in this.actionDic">
+        <td>{{action.action_ID}}</td>
+        <td>{{action.description}}</td>
+      </tr>
+      </tbody>
+    </table>
     <hr>
     <button v-on:click="goBack()">닫기</button>
   </div>
@@ -28,7 +33,13 @@ export default {
   name: "TestcaseEditComponent",
   data: function(){
     return {
-      actionDic: {}
+      actionDic: {},
+      square: {
+        x: 100,
+        y: 100
+      },
+      dragOffsetX: null,
+      dragOffsetY: null
     }
   },
   mounted: function(){
@@ -43,11 +54,25 @@ export default {
   methods: {
     goBack: function(){
       this.$router.go(-1)
+    },
+    drag: function({offsetX, offsetY}){
+      this.$refs.box.addEventListener('mousemove', this.move)
+    },
+    drop: function(){
+      this.$refs.box.removeEventListener('mousemove', this.move)
+    },
+    move: function({offsetX, offsetY}){
+      this.square.x = offsetX
+      this.square.y = offsetY
     }
   }
 }
 </script>
 
 <style scoped>
-
+.box {
+  margin: auto;
+  position: center;
+  display: block;
+}
 </style>
